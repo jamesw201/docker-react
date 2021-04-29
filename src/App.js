6da73d753806fa8751babe5aa2cbb2195655c2f6
james wilson <james.n.wilson@accenture.com>
2021-04-29 20:31:00 +0100

    fixed test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello James Wilson
+          learn react
         </a>
       </header>
     </div>
